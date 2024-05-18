32fd82b05c6dc403e1202ef842e8ad9514e63cf1
emanuele-f <black.silver@hotmail.it>
2024-05-18 16:53:58 +0200

    Set visibility of exported ushark functions
    This allows to strip the unused functions via gc-sections

--- libushark/ushark.h
+++ libushark/ushark.h
@@ -1,19 +1,21 @@
 #ifndef _USHARK_H_
 #define _USHARK_H_
 
+#define USHARK_EXPORT __attribute__((visibility("default")))
+
 typedef struct ushark ushark_t;
 typedef struct epan_dfilter dfilter_t;
 struct pcap_pkthdr;
 
-void ushark_init();
-void ushark_cleanup();
+USHARK_EXPORT void ushark_init();
+USHARK_EXPORT void ushark_cleanup();
 
-ushark_t* ushark_new(int pcap_encap, const char *dfilter);
-void ushark_destroy(ushark_t *sk);
-void ushark_set_pref(const char *name, const char *val);
-const char* ushark_dissect(ushark_t *sk, const unsigned char *buf, const struct pcap_pkthdr *hdr);
+USHARK_EXPORT ushark_t* ushark_new(int pcap_encap, const char *dfilter);
+USHARK_EXPORT void ushark_destroy(ushark_t *sk);
+USHARK_EXPORT void ushark_set_pref(const char *name, const char *val);
+USHARK_EXPORT const char* ushark_dissect(ushark_t *sk, const unsigned char *buf, const struct pcap_pkthdr *hdr);
 
 typedef void (*ushark_tls_data_callback)(const unsigned char *plain_data, unsigned int data_len);
-void ushark_dissect_tls(ushark_t *sk, const unsigned char *buf, const struct pcap_pkthdr *hdr, ushark_tls_data_callback cb);
+USHARK_EXPORT void ushark_dissect_tls(ushark_t *sk, const unsigned char *buf, const struct pcap_pkthdr *hdr, ushark_tls_data_callback cb);
 
 #endif
