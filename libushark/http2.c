78b9fa4bbf0ee2db8a29d694b2f9699609633e43
emanuele-f <black.silver@hotmail.it>
2025-12-26 11:10:13 +0100

    Handle multiple HTTP/2 streams into the same segment
    Fixes some HTTP reply data missing

--- libushark/http2.c
+++ libushark/http2.c
@@ -62,6 +62,8 @@ typedef enum {
 typedef struct {
     // http2 -> http2.stream -> http2.header / http2.body.fragments / http2.data.data
     http2_search_lvl cur_lvl;
+    ushark_http2_ctx_t *ctx;
+    conversation_t *conv;
     http2_reassembly_t *results;
     ushark_tls_data_callback tls_cb;
 } http2_tree_search_t;
@@ -114,6 +116,85 @@ extract_http2_header(proto_node *pn, gpointer data)
 }
 
 static void
+extract_http2_stream_id(proto_node *pn, gpointer data)
+{
+    guint32 *stream_id = (guint32*) data;
+    const char *node_key = pn->finfo->hfinfo->abbrev;
+
+    if (strcmp(node_key, "http2.streamid") == 0) {
+        *stream_id = fvalue_get_uinteger(&pn->finfo->value);
+    }
+}
+
+static void output_http2_message(http2_reassembly_t *res, ushark_tls_data_callback tls_cb) {
+    char *pre_headers = NULL;
+    char *tmp_buf = NULL;
+    size_t pre_headers_len = 0;
+
+    if (res->http2_hdrs.buf) {
+        if (res->http2_hdrs.status) {
+            // HTTP reply
+            int rv = asprintf(&tmp_buf, "HTTP/2.0 %s\r\n", res->http2_hdrs.status);
+
+            if (rv > 0) {
+                pre_headers_len = rv;
+                pre_headers = tmp_buf;
+            } else
+                pre_headers_len = 0;
+        } else if (res->http2_hdrs.method && res->http2_hdrs.authority) {
+            int rv = asprintf(&tmp_buf, "%s %s://%s%s HTTP/2.0\r\n",
+                res->http2_hdrs.method,
+                res->http2_hdrs.scheme ? res->http2_hdrs.scheme : "http",
+                res->http2_hdrs.authority,
+                res->http2_hdrs.path ? res->http2_hdrs.path : "/");
+
+            if (rv > 0) {
+                pre_headers_len = rv;
+                pre_headers = tmp_buf;
+            } else
+                pre_headers_len = 0;
+        } else {
+            pre_headers = res->http2_hdrs.buf;
+            pre_headers_len = res->http2_hdrs.buflen;
+        }
+    }
+
+    size_t tot_hdr_size = (pre_headers_len + res->headers_buflen > 0) ?
+        (pre_headers_len + res->headers_buflen + 2) : 0; // \r\n
+    size_t tot_size = tot_hdr_size + res->body_buflen;
+
+    if (tot_hdr_size > 0) {
+        guint8* assembly = (guint8*) malloc(tot_size);
+        guint8* p = assembly;
+
+        if (pre_headers_len > 0) {
+            memcpy(p, pre_headers, pre_headers_len);
+            p += pre_headers_len;
+        }
+        if (res->headers_buflen > 0) {
+            memcpy(p, res->headers_buf, res->headers_buflen);
+            p += res->headers_buflen;
+        }
+
+        *p++ = '\r';
+        *p++ = '\n';
+
+        if (res->body_buflen > 0) {
+            memcpy(p, res->body_buf, res->body_buflen);
+            p += res->body_buflen;
+        }
+
+        tls_cb(assembly, tot_size);
+
+        free(assembly);
+    } else if (res->body_buflen > 0)
+        tls_cb(res->body_buf, res->body_buflen);
+
+    if (tmp_buf)
+        free(tmp_buf);
+}
+
+static void
 search_http2_data(proto_node *pn, gpointer data)
 {
     http2_tree_search_t* ts = (http2_tree_search_t*) data;
@@ -123,23 +204,52 @@ search_http2_data(proto_node *pn, gpointer data)
     http2_reassembly_t *res = ts->results;
     const char *node_key = pn->finfo->hfinfo->abbrev;
 
-    bool go_deeper = false;
+    if (ts->cur_lvl == HTTP2_LVL_ROOT) {
+        if (strcmp(node_key, "http2") == 0) {
+            // go deeper
+            ts->cur_lvl++;
+            proto_tree_children_foreach(pn, search_http2_data, ts);
+            ts->cur_lvl--;
+        }
+    } else if (ts->cur_lvl == HTTP2_LVL_STREAM) {
+        if (strcmp(node_key, "http2.stream") == 0) {
+            // Extract stream ID from this stream node
+            guint32 stream_id = 0;
+            proto_tree_children_foreach(pn, extract_http2_stream_id, &stream_id);
 
-    if (ts->cur_lvl == HTTP2_LVL_ROOT)
-        go_deeper = (strcmp(node_key, "http2") == 0);
-    else if (ts->cur_lvl == HTTP2_LVL_STREAM) {
-        go_deeper = (strcmp(node_key, "http2.stream") == 0);
+            if (stream_id > 0) {
+                // Look up or create reassembly entry for this specific stream
+                guint32 reassembly_key = (ts->conv->conv_index << 10) | stream_id;
+                http2_reassembly_t *stream_res = (http2_reassembly_t*)
+                    g_hash_table_lookup(ts->ctx->reassembly_hash, GUINT_TO_POINTER(reassembly_key));
+
+                if (!stream_res) {
+                    stream_res = calloc(1, sizeof(http2_reassembly_t));
+                    g_hash_table_insert(ts->ctx->reassembly_hash, GUINT_TO_POINTER(reassembly_key), stream_res);
+                }
+
+                // Process this stream with its own reassembly context
+                http2_tree_search_t stream_ts = *ts;
+                stream_ts.results = stream_res;
+                stream_ts.cur_lvl++;
+                proto_tree_children_foreach(pn, search_http2_data, &stream_ts);
+
+                // Check if stream is complete and output if needed
+                if (stream_res->end_stream) {
+                    if (!stream_res->body_buf) {
+                        stream_res->body_buf = stream_res->data_buf;
+                        stream_res->body_buflen = stream_res->data_buflen;
+                    }
+
+                    output_http2_message(stream_res, ts->tls_cb);
+                    g_hash_table_remove(ts->ctx->reassembly_hash, GUINT_TO_POINTER(reassembly_key));
+                } else {
+                    stream_res->end_stream = false;
+                }
+            }
+        }
     } else { // HTTP2_LVL_HEADERS_BODY
-        if (strcmp(node_key, "http2.header") == 0) {
-            // in some cases there can be [DATA] blocks before [HEADERS]
-            // which are related to the previous request
-            if (res->body_buflen > 0)
-                ts->tls_cb(res->body_buf, res->body_buflen);
-            else if (res->data_buflen > 0)
-                ts->tls_cb(res->data_buf, res->data_buflen);
-            res->body_buf = res->data_buf = NULL;
-            res->body_buflen = res->data_buflen = 0;
-
+        if (res && (strcmp(node_key, "http2.header") == 0)) {
             http2_hdr_info hinfo = {};
             proto_tree_children_foreach(pn, extract_http2_header, &hinfo);
 
@@ -151,6 +261,24 @@ search_http2_data(proto_node *pn, gpointer data)
                 const guint8 *val_ptr = tvb_get_ptr(val_finfo->ds_tvb, val_finfo->start, val_finfo->length);
 
                 if (name_ptr && val_ptr && *name_ptr) {
+                    const char* name_ptr_c = (const char*) name_ptr;
+
+                    // Check if this is a new message (indicated by :method or :status pseudo-headers)
+                    // If we already have accumulated data, output it first
+                    if (*name_ptr == ':' &&
+                        (strcmp(name_ptr_c, ":method") == 0 || strcmp(name_ptr_c, ":status") == 0)) {
+                        if (res->http2_hdrs.buf || res->body_buflen > 0 || res->data_buflen > 0) {
+                            if (!res->body_buf) {
+                                res->body_buf = res->data_buf;
+                                res->body_buflen = res->data_buflen;
+                            }
+                            output_http2_message(res, ts->tls_cb);
+
+                            // Clear for next message
+                            memset(res, 0, sizeof(http2_reassembly_t));
+                        }
+                    }
+
                     size_t tot_len = name_finfo->length + 2 /*": "*/ + val_finfo->length + 2 /* \r\n */;
                     char **buf_ptr = NULL;
                     size_t *len_ptr;
@@ -222,12 +350,6 @@ search_http2_data(proto_node *pn, gpointer data)
                 res->end_stream = 1;
         }
     }
-
-    if (go_deeper) {
-        http2_tree_search_t deeper = *ts;
-        deeper.cur_lvl++;
-        proto_tree_children_foreach(pn, search_http2_data, &deeper);
-    }
 }
 
 ushark_http2_ctx_t* ushark_http2_init() {
@@ -244,99 +366,12 @@ void ushark_http2_cleanup(ushark_http2_ctx_t* ctx) {
 }
 
 void ushark_http2_process_data(ushark_http2_ctx_t *ctx, epan_dissect_t *edt, conversation_t *conv, ushark_tls_data_callback tls_cb) {
-  guint32 stream_id = http2_get_stream_id(&edt->pi);
-  guint32 reassembly_key = (conv->conv_index << 10) | stream_id;  // possible collisions, but unlikely
-  //printf("STREAM ID: %u.%u - %u\n", conv->conv_index, stream_id, reassembly_key);
-
-  http2_reassembly_t *res = (http2_reassembly_t*)
-      g_hash_table_lookup(ctx->reassembly_hash, GUINT_TO_POINTER(reassembly_key));
-
-  if (!res) {
-      res = calloc(1, sizeof(http2_reassembly_t));
-      g_hash_table_insert(ctx->reassembly_hash, GUINT_TO_POINTER(reassembly_key), res);
-  }
-
+  // Initialize tree search context
   http2_tree_search_t ts = {};
   ts.tls_cb = tls_cb;
-  ts.results = res;
+  ts.ctx = ctx;
+  ts.conv = conv;
+  ts.results = NULL;  // Will be set per-stream during traversal
 
   proto_tree_children_foreach(edt->tree, search_http2_data, &ts);
-
-  // NOTE: body_buf will be invalidated on next run, so check explicitly
-  if (res->end_stream) {
-      // Complete reassembly
-      char *pre_headers = NULL;
-      char *tmp_buf = NULL;
-      size_t pre_headers_len = 0;
-
-      if (!res->body_buf) {
-          res->body_buf = res->data_buf;
-          res->body_buflen = res->data_buflen;
-      }
-
-      if (res->http2_hdrs.buf) {
-          if (res->http2_hdrs.status) {
-              // HTTP reply
-              int rv = asprintf(&tmp_buf, "HTTP/2.0 %s\r\n", res->http2_hdrs.status);
-
-              if (rv > 0) {
-                  pre_headers_len = rv;
-                  pre_headers = tmp_buf;
-              } else
-                  pre_headers_len = 0;
-          } else if (res->http2_hdrs.method && res->http2_hdrs.authority) {
-              int rv = asprintf(&tmp_buf, "%s %s://%s%s HTTP/2.0\r\n",
-                  res->http2_hdrs.method,
-                  res->http2_hdrs.scheme ? res->http2_hdrs.scheme : "http",
-                  res->http2_hdrs.authority,
-                  res->http2_hdrs.path ? res->http2_hdrs.path : "/");
-
-              if (rv > 0) {
-                  pre_headers_len = rv;
-                  pre_headers = tmp_buf;
-              } else
-                  pre_headers_len = 0;
-          } else {
-              pre_headers = res->http2_hdrs.buf;
-              pre_headers_len = res->http2_hdrs.buflen;
-          }
-      }
-
-      size_t tot_hdr_size = (pre_headers_len + res->headers_buflen > 0) ?
-          (pre_headers_len + res->headers_buflen + 2) : 0; // \r\n
-      size_t tot_size = tot_hdr_size + res->body_buflen;
-
-      if (tot_hdr_size > 0) {
-          guint8* assembly = (guint8*) malloc(tot_size);
-          guint8* p = assembly;
-
-          if (pre_headers_len > 0) {
-              memcpy(p, pre_headers, pre_headers_len);
-              p += pre_headers_len;
-          }
-          if (res->headers_buflen > 0) {
-              memcpy(p, res->headers_buf, res->headers_buflen);
-              p += res->headers_buflen;
-          }
-
-          *p++ = '\r';
-          *p++ = '\n';
-
-          if (res->body_buflen > 0) {
-              memcpy(p, res->body_buf, res->body_buflen);
-              p += res->body_buflen;
-          }
-
-          tls_cb(assembly, tot_size);
-
-          free(assembly);
-      } else if (res->body_buflen > 0)
-          tls_cb(res->body_buf, res->body_buflen);
-
-      if (tmp_buf)
-        free(tmp_buf);
-
-      g_hash_table_remove(ctx->reassembly_hash, GUINT_TO_POINTER(reassembly_key));
-  } else
-      res->end_stream = false;
 }
